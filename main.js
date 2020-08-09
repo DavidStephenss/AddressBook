52b8f8066b5f8f05f4f3097b2361ced99aeb4a08
DavidStephenss <dvdstephensd@gmail.com>
2020-08-09 16:02:04 -0500

    working on buttonstuff

--- main.js
+++ main.js
@@ -3,25 +3,40 @@ window.onload = function () {
 }
 
 Const getOurUsers = () =>{
-fetch("https://randomuser.me/api/?results=10&inc=gender,name,nat,picture")
+fetch("https://randomuser.me/api/?results=10&inc=gender,name,picture,email,phone,location,dob")
 .then((res) => res.json())
 .then((users) => (arrayOfUsers = users.results))
 }
-
-const displayUsers = () => {
-  const allUsers = document.getElementById("all-users")
-  arrayOfUsers.map((users) => {
-    const li = document.createElement("li")
-    const img = document.createElement("img")
-    img.src = picture.large
-    const text = document.createTextNode(
-      `#${index}, Title: ${post.name.first}:  ${post.name.last}`
-    )
-
-    li.appendChild(text)
-    li.appendChild(img)
-    allUsers.append(li)
-  })
+//should hide allinfo until clicked.
+const hideInfo = (id) => {
+const allInfo = document.getElementById(id)
+if (p.style.display === "block") {
+    p.style.display = "none"
+  } else {
+    p.style.display = "block"
 }
 
 
+
+const displayUsers = () => {
+  const allUsers = document.getElementById("all-users")
+  arrayOfUsers.map((users, i) => {
+    const li = document.createElement("li")
+    const { name, phone, email, dob, location, picture } = users
+    const img = document.createElement("img")
+    img.src = picture.large
+    const buttonStuff = document.createElement("button")
+    const words = document.createTextNode("extra info")
+    const p = document.createElement("p")
+    p.setAttribute("id", 'more-info-${i}')
+    buttonStuff.addEventListener("click", () => {
+  })
+      const fullName = document.createTextNode( '${name.first} ${name.last}')
+      // const button = document.createElement("button")
+      const allInfo = document.createTextNode( 'Phone: ${phone}, Email: ${email}, Address: ${location.street.number} ${location.street.name} ${location.city} ${location.state} ${location.postcode} ${location.country}, Birthday: ${dob.date}')
+    li.appendChild(img)
+    allUsers.append(li)
+    li.appendChild(fullName)
+    p.appendChild(allInfo)
+  })
+}
