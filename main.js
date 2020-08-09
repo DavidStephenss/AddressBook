c610ef0066f248e19c947eca9998cb0b56fa4166
DavidStephenss <dvdstephensd@gmail.com>
2020-08-09 17:01:18 -0500

    done but not done?

--- main.js
+++ main.js
@@ -1,26 +1,28 @@
+let arrayOfUsers
 window.onload = function () {
   getOurUsers()
 }
-
-Const getOurUsers = () =>{
-fetch("https://randomuser.me/api/?results=10&inc=gender,name,picture,email,phone,location,dob")
+//uses fetch to get random people from the random user API.
+//get 10 peoples gender, name, picture,email, phone,location, and dob
+const getOurUsers = () => {
+fetch("https://randomuser.me/api/?results=10&inc=name,picture,email,phone,location,dob")
 .then((res) => res.json())
 .then((users) => (arrayOfUsers = users.results))
 }
-//should hide allinfo until clicked.
+//should hide all info until clicked.
 const hideInfo = (id) => {
 const allInfo = document.getElementById(id)
-if (p.style.display === "block") {
-    p.style.display = "none"
+if (allInfo.style.display === "block") {
+    allInfo.style.display = "none"
   } else {
-    p.style.display = "block"
+    allInfo.style.display = "block"
 }
 
 
-
+//displays all the users and info
 const displayUsers = () => {
   const allUsers = document.getElementById("all-users")
-  arrayOfUsers.map((users, i) => {
+  arrayOfUsers.map((users) => {
     const li = document.createElement("li")
     const { name, phone, email, dob, location, picture } = users
     const img = document.createElement("img")
@@ -28,15 +30,23 @@ const displayUsers = () => {
     const buttonStuff = document.createElement("button")
     const words = document.createTextNode("extra info")
     const p = document.createElement("p")
+    p.style.display = "none"
     p.setAttribute("id", 'more-info-${i}')
     buttonStuff.addEventListener("click", () => {
+      moreInfo(p.id)
   })
-      const fullName = document.createTextNode( '${name.first} ${name.last}')
-      // const button = document.createElement("button")
-      const allInfo = document.createTextNode( 'Phone: ${phone}, Email: ${email}, Address: ${location.street.number} ${location.street.name} ${location.city} ${location.state} ${location.postcode} ${location.country}, Birthday: ${dob.date}')
+      const fullName = document.createTextNode('${user.name.first} ${user.name.last}')
+      const allInfo = document.createTextNode('Phone: ${phone}, Email: ${email}, Address: ${location.street.number}, Birthday: ${dob.date}')
+
     li.appendChild(img)
     allUsers.append(li)
     li.appendChild(fullName)
     p.appendChild(allInfo)
+    allUsers.append(buttonStuff)
+    buttonStuff.append(words)
   })
+}
+ 
+const consoleUser = () => {
+  console.log(arrayOfUsers)
 }
